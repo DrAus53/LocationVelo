eb24bd771cc325cef5f318fd4c4a8c678dfe14ce
Lucien <lucien.christian@reseau.eseo.fr>
2018-03-14 11:20:57 +0100

    Creation Velo and ReservationVelo classes

---
+++ LocationVelo/src/fr/eseo/jee/agence/velo/ReservationVelo.java
@@ -0,0 +1,53 @@
+package fr.eseo.jee.agence.velo;
+
+import java.util.Date;
+
+public class ReservationVelo {
+
+	int codeReservation ;
+	int codeVelo ;
+	int codeClient ;
+	Date dateDebut ;
+	Date dateFin ;
+	boolean paiementEffectue ;
+	
+	
+	public int getCodeReservation() {
+		return codeReservation;
+	}
+	public void setCodeReservation(int codeReservation) {
+		this.codeReservation = codeReservation;
+	}
+	public int getCodeVelo() {
+		return codeVelo;
+	}
+	public void setCodeVelo(int codeVelo) {
+		this.codeVelo = codeVelo;
+	}
+	public int getCodeClient() {
+		return codeClient;
+	}
+	public void setCodeClient(int codeClient) {
+		this.codeClient = codeClient;
+	}
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+	public Date getDateFin() {
+		return dateFin;
+	}
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+	public boolean isPaiementEffectue() {
+		return paiementEffectue;
+	}
+	public void setPaiementEffectue(boolean paiementEffectue) {
+		this.paiementEffectue = paiementEffectue;
+	}
+	
+	
+}
--- LocationVelo/src/fr/eseo/jee/agence/velo/Velo.java
+++ LocationVelo/src/fr/eseo/jee/agence/velo/Velo.java
@@ -2,4 +2,36 @@ package fr.eseo.jee.agence.velo;
 
 public class Velo {
 
+	int codeVelo ;
+	String categorie ;
+	String ville ;
+	double prixLocation ;
+	
+	
+	public int getCodeVelo() {
+		return codeVelo;
+	}
+	public void setCodeVelo(int codeVelo) {
+		this.codeVelo = codeVelo;
+	}
+	public String getCategorie() {
+		return categorie;
+	}
+	public void setCategorie(String categorie) {
+		this.categorie = categorie;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public double getPrixLocation() {
+		return prixLocation;
+	}
+	public void setPrixLocation(double prixLocation) {
+		this.prixLocation = prixLocation;
+	}
+	
+	
 }
