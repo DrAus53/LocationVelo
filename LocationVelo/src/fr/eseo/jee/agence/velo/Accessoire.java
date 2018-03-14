f9faf060bf39ec825264c64a07a763b7671655f2
Lucien <lucien.christian@reseau.eseo.fr>
2018-03-14 11:46:59 +0100

    Creation Accessoire and Client class

---
+++ LocationVelo/src/fr/eseo/jee/agence/velo/Accessoire.java
@@ -0,0 +1,22 @@
+package fr.eseo.jee.agence.velo;
+
+public class Accessoire {
+
+	int codeAccessoire ;
+	String type ;
+	
+	public int getCodeAccessoire() {
+		return codeAccessoire;
+	}
+	public void setCodeAccessoire(int codeAccessoire) {
+		this.codeAccessoire = codeAccessoire;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
